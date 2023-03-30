053998cb7827d61f7219cffa7ee93f6a146cf6f3
Paul Izekor <izekorpaul0@gmail.com>
2023-03-31 00:12:48 +0100

    pulling from main branch

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 function About() {
+  const mission = `Our Mission is Simple: To help you search, apply and secure jobs that help you advance your career without stress`
   return (
     <div className='about'>
-      
+      <div className="blur">
+        <h1 className="header-text">
+          {mission}
+        </h1>
+      </div>
     </div>
   )
 }
